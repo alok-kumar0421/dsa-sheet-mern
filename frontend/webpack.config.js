68a6ee375a0f12dcb7680c856469ecce723ad73e
alok kumar0421 <aloknokha01@gmail.com>
2025-12-06 04:28:09 +0530

    Added final webpack config + redirects fixing 404 refresh

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,8 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const webpack = require("webpack");
-require("dotenv").config({ path: "./.env" }); // <--- important
 
 module.exports = {
   entry: "./src/index.js",
@@ -32,12 +30,11 @@ module.exports = {
     extensions: [".js", ".jsx"]
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: "./public/index.html" }),
-    new CopyWebpackPlugin({
-      patterns: [{ from: "public/_redirects" }] 
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
     }),
-    new webpack.DefinePlugin({
-      "process.env.REACT_APP_API_URL": JSON.stringify(process.env.REACT_APP_API_URL)
+    new CopyWebpackPlugin({
+      patterns: [{ from: "public/_redirects", to: "" }]
     })
   ]
 };
